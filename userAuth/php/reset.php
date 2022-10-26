2e6f272cef24f5d480543907edf1385a22f35229
fyneny <fyneny92@gmail.com>
2022-10-26 17:41:17 +0100

    Update reset.php

--- userAuth/php/reset.php
+++ userAuth/php/reset.php
@@ -4,27 +4,25 @@ if(isset($_POST['submit'])){
     $newpassword = $_POST['password'];
  
     resetPassword($email, $newpassword);
-}
+} 
 
 function resetPassword($email, $password){
-    //open file and check if the username exist inside
-    //if it does, replace the password
-   $file = fopen("../storage/users.csv","r") ;
-   while (!feof ($file)){
-    $line = fgetcsv ($file);
-    if ($line[1] == $email){
-        $line [2] = $password;
-        fclose($file);
-        $file = fopen('../storage/users.csv', 'w');
-        fputcsv($file);
-        fclose($file);
-        echo "password reset successful";
-        exit();
+   $file = fopen('../storage/users.csv','r');
+   while(!feof($file)){
+     $line = fgetcsv($file); 
+     if($line[1] == $email){
+         $line [2] = $password;
+         fclose($file);
+         $file = fopen('../storage/users.csv', 'w');
+         fputcsv($file, $line);
+         fclose($file);
+         echo "PASSWORD RESET SUCCESSFUL";
+         exit();
+        }
     }
-   }
 }
-echo "invalid username or password";
-fclose($file);
+  echo "Invalid username or password";
+  fclose($file);
 
 
 
